327896397898ae9731842fd4fdb567c4e0712476
colpas <62711290+colpas@users.noreply.github.com>
2022-02-21 12:19:59 +0000

    Route ending for WCA

--- app/routes.js
+++ app/routes.js
@@ -2,5 +2,17 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+router.post("/wca-htl-975-new-end/recommendation/decision-post", function(req, res, next){
 
+if(req.body['recommendation'] === 'Limited capability for work (LCW)'){
+    res.redirect('/wca-htl-975-new-end/lcw');
+} else if(req.body['recommendation'] === 'Limited capability for work-related activity (LCWRA)'){
+res.redirect('/wca-htl-975-new-end/lcwra')
+} else {
+    res.redirect('/wca-htl-975-new-end/fit-for-work');
+}
+next();
+})
+
+// console.log('**************',req.path)
 module.exports = router
