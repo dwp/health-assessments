a9c30ec7afb513bf0c8b304d0ae5fa9c7cb011ff
colpas <62711290+colpas@users.noreply.github.com>
2022-12-15 08:14:51 +0000

    fixes to tabs and bug on split scree

--- public/javascripts/recoBuilder.js
+++ public/javascripts/recoBuilder.js
@@ -1,30 +1,26 @@
 if(document.getElementById('recoBuilderFrame')) {
-
     window.addEventListener('resize', manaegSidePanel);
-
     document.querySelectorAll('.reco-builder-page-link').forEach((element) => {
         element.addEventListener('click', changeSidePage);
     });
-
     document.querySelectorAll('.reco-toggle-icon').forEach((element) => {
         element.addEventListener('click', togglePane);
     });
-
     document.querySelectorAll('#splitScreenLink').forEach((element) => {
         element.addEventListener('click', toggleSplitScreen);
     });
-
     document.querySelectorAll('.split-screen-off').forEach((element) => {
         element.addEventListener('click', turnOffSplitScreen);
     });
-
     function addDocumentEventListeners() {
         document.querySelectorAll('.reco-document').forEach((element) => {
             element.addEventListener('click', loadDocument);
         });
     }
-
     function populateSidePage(url, isDocument) {
+        if(getCurrentTab() === 'documentsLink') {
+            return;
+        }
         url = url || document.getElementById('recoBuilderFrame').dataset.url;
         fetch(url)
         .then(function(response) {
@@ -71,12 +67,13 @@ if(document.getElementById('recoBuilderFrame')) {
         }
     }
 
-    // <input type="hidden" name="previousDocument" id="previousDocument" value="{{ data.openDocument }}">
     function toggleSplitScreen(e) {
         e.preventDefault();
         console.log(e.target.href);
         if(document.getElementById('recoRightPane')) {
             document.getElementById('splitScreenLink').setAttribute("aria-current", "page");
+            document.getElementById('assessmentLink').removeAttribute("aria-current");
+            document.getElementById('documentsLink').removeAttribute("aria-current");
             document.getElementById('recoLeftPane').classList.remove('govuk-grid-column-two-thirds');
             document.getElementById('recoLeftPane').classList.add('govuk-grid-column-two-fifths');
             document.getElementById('recoRightPane').classList.remove('reco-hidden');
@@ -89,13 +86,13 @@ if(document.getElementById('recoBuilderFrame')) {
         })
             .catch((error) => console.error('Error:', error));
             location.href = e.target.href;
-
     }
 
     function turnOffSplitScreen(e) {
         e.preventDefault();
         if(document.getElementById('recoRightPane')) {
             document.getElementById('splitScreenLink').removeAttribute("aria-current");
+            document.getElementById(getCurrentTab()).setAttribute("aria-current", "page");
             document.getElementById('recoLeftPane').classList.remove('govuk-grid-column-two-fifths');
             document.getElementById('recoLeftPane').classList.add('govuk-grid-column-two-thirds');
             document.getElementById('recoRightPane').classList.add('reco-hidden');
@@ -107,9 +104,7 @@ if(document.getElementById('recoBuilderFrame')) {
             body: JSON.stringify({ field: 'splitScreen', value: 'false' }),
         })
             .catch((error) => console.error('Error:', error));
-            console.log(e.target);
             location.href = e.target.href;
-
     }
 
     if(document.getElementById('splitScreenLink') && document.getElementById('recoRightPane')) {
@@ -118,11 +113,14 @@ if(document.getElementById('recoBuilderFrame')) {
             document.getElementById('recoLeftPane').classList.add('govuk-grid-column-two-fifths');
             document.getElementById('recoRightPane').classList.remove('reco-hidden');
             document.getElementById('splitScreenLink').setAttribute("aria-current", "page");
+            document.getElementById('assessmentLink').removeAttribute("aria-current");
+            document.getElementById('documentsLink').removeAttribute("aria-current");
         } else if(document.getElementById('splitScreenLink').dataset.isToggledOn === 'false' && !document.getElementById('recoRightPane').classList.contains('reco-hidden')) {
             document.getElementById('recoLeftPane').classList.remove('govuk-grid-column-two-fifths');
             document.getElementById('recoLeftPane').classList.add('govuk-grid-column-two-thirds');
             document.getElementById('recoRightPane').classList.add('reco-hidden');
             document.getElementById('splitScreenLink').removeAttribute("aria-current");
+            document.getElementById(getCurrentTab()).setAttribute("aria-current", "page");
         }
     }
 
@@ -130,10 +128,6 @@ if(document.getElementById('recoBuilderFrame')) {
         const isLeftArrow = e.target.id === 'recoLeftIcon';
         document.getElementById('recoLeftIcon').classList.toggle('reco-hidden');
         document.getElementById('recoRightIcon').classList.toggle('reco-hidden');
-
-       
-
-
         if(!isLeftArrow) {
             // govuk-grid-column-one-third
             document.getElementById('recoLeftPane').classList.remove('govuk-grid-column-two-fifths');
@@ -155,7 +149,6 @@ if(document.getElementById('recoBuilderFrame')) {
     function loadDocument(e) {
         e.preventDefault();
         populateSidePage(e.target.href, true);
-        // console.log(e.target);
         recordOpenDocument(e.target.href);
     }
 
@@ -180,10 +173,23 @@ if(document.getElementById('recoBuilderFrame')) {
             .catch((error) => console.error('Error:', error));
     }
 
-    if(document.getElementById('previousDocument').value) {
+    if(document.getElementById('previousDocument') && document.getElementById('previousDocument').value) {
         populateSidePage(document.getElementById('previousDocument').value, true)
     } else {
         populateSidePage();
     }
 
+    function getCurrentTab() {
+        if(location.href.includes('claimant-documents') || location.href.includes('further-evidence')) {
+            return 'documentsLink';
+        }
+        return 'assessmentLink';
+    }
+
+    if(getCurrentTab() === 'documentsLink') {
+        document.getElementById('documentsLink').setAttribute("aria-current", "page");
+        document.getElementById('splitScreenLink').removeAttribute("aria-current");
+        document.getElementById('assessmentLink').removeAttribute("aria-current");
+    }
+    
   }
